9ec40660324f21d09dc3d4b095fddb9dcbd7e0e1
SiobhanG26 <118477071+SiobhanG26@users.noreply.github.com>
2022-12-31 00:13:06 +0000

    Added the functionality for the select

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElement.innerHTML = londonTime.format("Do MMMM YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("Do MMMM YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+            <h2> ${cityTimezone}</h2>
+            <div class="date">${cityTime.format("Do MMMM YYYY")}</div>
+          </div>
+            <div class="time">${cityTime.format(
+              "h:mm:ss"
+            )}<small>${cityTime.format("A")}</small></div>
+        </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#selected-city");
+citiesSelectElement.addEventListener("change", updateCity);
